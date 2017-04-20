15ad2fef4dc09516eff5c92d8c68a13dabf623f8
nwre87 <nwre87@gmail.com>
2017-04-19 14:52:09 -1000

    add eslint and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const webpack = require('webpack');
+const path = require('path');
 
 const config = {
-  
-}
+  context: path.join(__dirname, 'src'),
+  entry: 'index.js',
+  output: {
+    path: path.join(__dirname, 'src'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {},
+};
+
+module.exports = config;
