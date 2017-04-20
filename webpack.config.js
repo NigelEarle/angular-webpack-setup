09a182f30d2c4dd7cc452103a783fe391d446220
nwre87 <nwre87@gmail.com>
2017-04-19 15:28:33 -1000

    add webpack module loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,51 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const nodeModules = path.resolve(__dirname, 'node_modules');
+
 const config = {
   context: path.join(__dirname, 'src'),
-  entry: 'index.js',
+  entry: './index.js',
   output: {
-    path: path.join(__dirname, 'src'),
+    path: path.join(__dirname, '/build/'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   devtool: 'source-map',
-  module: {},
+  plugins: [
+    new webpack.DefinePlugin(),
+    new webpack.ProvidePlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: nodeModules,
+        loaders: 'babel-loader',
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.s?css$/,
+        exclude: nodeModules,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'autoprefixer-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        exclude: nodeModules,
+        loader: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.html$/,
+        exclude: nodeModules,
+        loader: 'raw-loader',
+      },
+    ],
+  },
 };
 
 module.exports = config;
